4cdff2226aed707d58e2c8c2f3b2fc37b7db663b
ivandmitrienko <oneblackcrystal@gmail.com>
2022-04-28 23:31:56 +0300

    initial commit

---
+++ Lesson2(RC299. Домашнее задание — развитие проекта ishop)/Product.js
@@ -0,0 +1,52 @@
+let Product = React.createClass({
+
+    displayName: 'iShop',
+
+    propTypes: {
+        id: React.PropTypes.number.isRequired,
+        count: React.PropTypes.number.isRequired,
+        price: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string.isRequired,
+        url: React.PropTypes.string.isRequired,
+        remove: React.PropTypes.func,
+        clicked: React.PropTypes.func,
+        isClicked: React.PropTypes.bool,
+    },
+
+
+    clicked: function(){
+
+        this.props.clicked(this.props.id);
+        
+    },
+
+    remove: function(){
+
+        this.props.remove(this.props.id, this.props.title);
+
+    },
+    
+    
+    render() {
+
+        let bgProduct = this.props.isClicked ? {backgroundColor: 'orange'} : null;
+
+        return React.DOM.tr({
+             className:" product_row",
+             style: bgProduct,
+             onClick: this.clicked,
+            },
+            React.DOM.td(null, React.DOM.a({href: this.props.url},
+                React.DOM.span({className: 'product_image', style: {backgroundImage: `url("${this.props.url}")`}}))
+            ),
+            React.DOM.td(null, this.props.title),
+            React.DOM.td(null, this.props.price),
+            React.DOM.td(null, this.props.count),
+            React.DOM.td(null,  React.DOM.button(
+                {className: 'product_remove', type: 'button', value: this.props.id, onClick: this.remove}, 'Remove'
+            )),
+
+        )
+    }
+
+});
---
+++ Lesson2(RC299. Домашнее задание — развитие проекта ishop)/iShop.js
@@ -0,0 +1,76 @@
+let iShop = React.createClass({
+
+    displayName: 'iShop',
+
+    getDefaultProps: function () {
+        return {
+            iShopName: "Проверьте название магазина",
+            iShopArr: [{ id: 0, title: 'нет данных', price: 'нет данных', url: 'нет данных', count: 'нет данных' }]
+        }
+    },
+
+    propTypes: {
+        iShopName: React.PropTypes.string.isRequired,
+        iShopArr:React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+              id: React.PropTypes.number.isRequired,
+              count: React.PropTypes.number.isRequired,
+              price: React.PropTypes.number.isRequired,
+              title: React.PropTypes.string.isRequired,
+              url: React.PropTypes.string.isRequired,
+            })
+        )
+    }, 
+
+    getInitialState: function () {
+        return {
+            products: this.props.iShopArr,
+            isClicked: 0,
+        }
+    },
+
+    clickedItem: function(id){
+
+        this.setState({isClicked:id});
+
+    },
+
+    removeItem: function(id, title){
+        if (confirm(`Вы хотите удалить ${title.toUpperCase()}?`)) {
+            let filtered = this.state.products.filter(product => product.id !== id);
+            this.setState({books: filtered}, null);
+        }
+    },
+
+    render() {
+        let products = this.state.products.map(product =>
+            React.createElement(Product, {
+                key: product.id,
+                id: product.id,
+                title: product.title,
+                url: product.url,
+                price: product.price,
+                count: product.count,
+                remove: this.removeItem,
+                clicked: this.clickedItem,
+                isClicked: product.id === this.state.isClicked,
+            })
+        );
+
+        return React.DOM.div({className: 'shop'},
+            React.DOM.table({className: 'shop_table'},
+                React.DOM.caption({className: 'shop_name'}, this.props.shopName),
+                React.DOM.tbody(null,
+                    React.DOM.tr({className: 'shop_column_name'},
+                        React.DOM.th(null, 'URL фотографии'),
+                        React.DOM.th(null, 'Название'),
+                        React.DOM.th(null, 'Цена'),
+                        React.DOM.th(null, 'Количество'),
+                        React.DOM.th(null, 'Контроль'),
+                    ),
+                    products
+                )
+            )
+        )
+    },
+});    
