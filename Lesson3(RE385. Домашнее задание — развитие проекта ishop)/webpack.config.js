e6c5a4b08cfeed657c3aa0f4e13b3e963294b61b
ivandmitrienko <oneblackcrystal@gmail.com>
2022-05-11 14:34:30 +0300

    initial commit

--- Lesson3(RE385. Домашнее задание — развитие проекта ishop)/webpack.config.js
+++ Lesson3(RE385. Домашнее задание — развитие проекта ishop)/webpack.config.js
@@ -3,18 +3,23 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractCSS = new ExtractTextPlugin({
-    filename: "build.css"
+    filename: "bundle.css"
 });
 
 module.exports = { 
     entry: "./App.js", // основной файл приложения
     output:{ 
         path: __dirname, // путь к каталогу выходных файлов
-        filename: "build.js"  // название создаваемого файла 
+        filename: "bundle.js"  // название создаваемого файла 
     }, 
     devtool:'source-map',
     module:{ 
         rules:[
+            { 
+                test: /\.jsx?$/, // какие файлы обрабатывать
+                exclude: /node_modules/, // какие файлы пропускать
+                use: { loader: "babel-loader" }
+            },
             {
                 test: /\.css$/,
                 use: extractCSS.extract({
