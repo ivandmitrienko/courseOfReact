c139bf7a958b9a20dbaf794ad8b964268ec744e5
ivandmitrienko <oneblackcrystal@gmail.com>
2022-05-16 22:16:55 +0300

    initial commit

---
+++ Lesson3(RE385_ishop3) copy/App.js
@@ -0,0 +1,19 @@
+"use strict"
+
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import IShop from './components/iShop';
+
+let iShopName='Green';
+
+let iShopArr=require('./iShopArr.json');
+
+ReactDOM.render(
+  <IShop
+    ShopName = {iShopName}
+    ShopArr = {iShopArr}
+  />
+  , document.getElementById('container') 
+);
