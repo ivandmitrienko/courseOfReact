cc4bf321ff4f8869488b7f54d2d8af7f4662e486
ivandmitrienko <oneblackcrystal@gmail.com>
2022-05-17 23:27:45 +0300

    initial commit

--- Lesson4(RainbowFrame)/components/RainbowFrame.js
+++ Lesson4(RainbowFrame)/components/RainbowFrame.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default (props) =>{
     const frames = props.colors.reduce((acc,cur)=>
-    <div style={{border:`2px solid ${cur}`, padding:'10px'}}>
-        {cur}
-    </div>,props.children);
+    <div style={{border:`2px solid ${cur}`, padding:'10px', textAlign:'center'}}>
+        {acc}
+    </div>,
+    props.children);
 
     return <div>{frames}</div>;
 }
