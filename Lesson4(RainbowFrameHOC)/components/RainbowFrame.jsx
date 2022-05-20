9f557752775ade04c7e48e9c96b502a0d9026877
ivandmitrienko <oneblackcrystal@gmail.com>
2022-05-20 16:39:21 +0300

    initial commit

--- Lesson4(RainbowFrameHOC)/components/RainbowFrame.jsx
+++ Lesson4(RainbowFrameHOC)/components/RainbowFrame.jsx
@@ -1,12 +1,12 @@
-// import React from 'react';
+import React from 'react';
 
-// export default (props) =>{
-//     const frames = props.colors.reduce((acc,cur)=>
-//     <div style={{border:`2px solid ${cur}`, padding:'10px', textAlign:'center'}}>
-//         {acc}
-//     </div>,
-//     props.children);
+export default (props) =>{
+    const frames = props.colors.reduce((acc,cur)=>
+    <div style={{border:`2px solid ${cur}`, padding:'10px', textAlign:'center'}}>
+        {acc}
+    </div>,
+    props.children);
 
-//     return <div>{frames}</div>;
-// }
+    return <div>{frames}</div>;
+}
 
