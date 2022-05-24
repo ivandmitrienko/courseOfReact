aaed81d9f87f7182a444537459d2dcc1e561edc5
ivandmitrienko <oneblackcrystal@gmail.com>
2022-05-24 23:04:21 +0300

    initial commit

---
+++ Lesson5(Mobile)/componets/MobileClient/MobileClient.js
@@ -0,0 +1,73 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { mobileEvents } from '../../events';
+
+import('./MobileClient.css');
+
+class MobileClient extends React.PureComponent {
+
+  static propTypes = {
+    client: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      FIO: PropTypes.shape({
+        fam: PropTypes.string.isRequired,
+        im: PropTypes.string.isRequired,
+        otch: PropTypes.string.isRequired,
+      }),
+      balance: PropTypes.number.isRequired,
+    })
+  };
+
+  state = {
+    ...this.props.client
+  };
+
+  componentDidUpdate = (prevProps) => {
+    const isNewProps = this.props.client.id !== prevProps.client.id ||
+      this.props.client.balance !== prevProps.client.balance ||
+      this.props.client.FIO.fam !== prevProps.client.FIO.fam ||
+      this.props.client.FIO.im !== prevProps.client.FIO.im ||
+      this.props.client.FIO.otch !== prevProps.client.FIO.otch;
+
+    if (isNewProps) {
+      this.setState({
+        ...this.props.client,
+        FIO: {
+          ...this.props.client.FIO
+        },
+      });
+    }
+  };
+
+
+  delete = (EO) => {
+    mobileEvents.emit('EDelete', this.state.id);
+  }
+
+  edit = () => {
+    mobileEvents.emit('EEdit', this.state);
+  }
+
+  render() {
+
+    console.log("MobileClient id=" + this.props.client.id + " render");
+
+    const isActive = this.state.balance > 0;
+
+    return (
+      <tr>
+        <td>{this.state.FIO.fam}</td>
+        <td>{this.state.FIO.im}</td>
+        <td>{this.state.FIO.otch}</td>
+        <td>{this.state.balance}</td>
+        <td style={{ backgroundColor: isActive ? 'chartreuse' : 'red' }} >
+          {isActive ? 'Active' : 'Blocked'}
+        </td>
+        <td><button className='MobileClient-Edit' onClick={this.edit} >Редактировать</button></td>
+        <td><button className='MobileClient-Delete' onClick={this.delete}>Удалить</button></td>
+      </tr>
+    )
+  }
+}
+
+export default MobileClient;
--- Lesson5(Mobile)/index.js
+++ Lesson5(Mobile)/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+"use strict";
 
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App.jsx';
+import MobileCompany from './components/MobileCompany/MobileCompany';
+
+let companyName = 'A1';
+let clientsArr = [
+  { id: 101, FIO: { fam: "Иванов", im: "Иван", otch: "Иванович" }, balance: 200 },
+  { id: 105, FIO: { fam: "Сидоров", im: "Сидор", otch: "Сидорович" }, balance: 250 },
+  { id: 110, FIO: { fam: "Петров", im: "Пётр", otch: "Петрович" }, balance: 180 },
+  { id: 120, FIO: { fam: "Григорьев", im: "Григорий", otch: "Григорьевич" }, balance: -220 },
+];
 
 ReactDOM.render(
-    <App/>
-    , document.getElementById('container') 
-  );
+  <MobileCompany
+    name={companyName}
+    clients={clientsArr}
+  />
+  , document.getElementById('container')
+);
