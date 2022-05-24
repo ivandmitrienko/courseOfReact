026c71334c98dd541d0ac4817affacb95ca15bde
ivandmitrienko <oneblackcrystal@gmail.com>
2022-05-25 00:29:54 +0300

    initial commit

--- Lesson5(Mobile)/components/MobileCompany/MobileCompany.js
+++ Lesson5(Mobile)/components/MobileCompany/MobileCompany.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import mobileEvents from "../../events";
+import {mobileEvents} from "../../events";
 
 import MobileClient from '../MobileClient/MobileClient';
 import MobileForm from '../MobileForm/MobileForm';
